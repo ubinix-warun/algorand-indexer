283e6be7a6406e8d961d483f30e6f295a3338cbc
algochoi <86622919+algochoi@users.noreply.github.com>
2022-01-09 21:37:30 -0500

    Test inner txn parsing

--- idb/postgres/postgres.go
+++ idb/postgres/postgres.go
@@ -935,14 +935,13 @@ func (db *IndexerDb) yieldTxnsThreadSimple(ctx context.Context, rows pgx.Rows, r
 					err = fmt.Errorf("error decoding roottxn, err: %w", err)
 					row.Error = err
 				}
-			} else {
-				// Root transaction.
-				row.Txn = new(transactions.SignedTxnWithAD)
-				*row.Txn, err = encoding.DecodeSignedTxnWithAD(txn)
-				if err != nil {
-					err = fmt.Errorf("error decoding txn, err: %w", err)
-					row.Error = err
-				}
+			}
+			// Root transaction.
+			row.Txn = new(transactions.SignedTxnWithAD)
+			*row.Txn, err = encoding.DecodeSignedTxnWithAD(txn)
+			if err != nil {
+				err = fmt.Errorf("error decoding txn, err: %w", err)
+				row.Error = err
 			}
 
 			row.RoundTime = roundtime
